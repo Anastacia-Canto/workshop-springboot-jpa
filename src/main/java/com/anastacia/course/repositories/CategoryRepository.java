df4ce7c9393cc97a239f4cc3ba4ccb082495e1f1
Anastacia <anaanestesia@gmail.com>
2023-09-01 14:47:35 +0100

    creating Category

---
+++ src/main/java/com/anastacia/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.anastacia.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anastacia.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
